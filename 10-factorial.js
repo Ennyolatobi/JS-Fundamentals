22c33be7bb02e0dc51d9f7d4f451ee6567e672e1
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-09-12 16:40:43 +0100

    Add a script that computes and prints a factorial

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2);
+const n = parseInt(args[0], 10);
+
+function factorial(x) {
+    if (Number.isNaN(x)) return 1;
+
+    if (x <= 1) return 1;
+
+    return x * factorial(x - 1);
+}
+
+console.log(factorial(n));
